838c0da945d937b98a4053ae3fd38b94564eaf6f
Stanislav.Yusko <sayusko86@gmail.com>
2016-12-08 18:38:56 +0200

    #

---
+++ web/js/calendar.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+        // options
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay,listWeek'
+        },
+        defaultDate: '2016-12-11',
+        navLinks: true,
+        editable: false,
+        eventLimit: true,
+        events: [
+            {
+                id: 1,
+                title: 'Lesson',
+                start: '2016-12-13T18:00:00',
+                end: '2016-12-13T20:15:00'
+            },
+            {
+                id: 1,
+                title: 'Lesson',
+                start: '2016-12-20T18:00:00',
+                end: '2016-12-20T20:15:00'
+            },
+            {
+                id: 1,
+                title: 'Lesson',
+                start: '2016-12-27T18:00:00',
+                end: '2016-12-27T20:15:00'
+            },
+            {
+                id: 1,
+                title: 'Lesson',
+                start: '2017-01-03T18:00:00',
+                end: '2017-01-03T20:15:00'
+            }
+        ]
+    })
+
+});
